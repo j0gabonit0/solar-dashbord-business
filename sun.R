784340622500c2d56d8d9125482609733bc910c6
saschabandura <s-bandura@posteo.de>
2020-08-08 17:30:56 +0200

    -

--- sun.R
+++ sun.R
@@ -98,11 +98,27 @@ sun <- sun %>%
 
 
 #############
-latitude = 40
-longitude = -100
+latitude = 51
+longitude = 7.4
 tilt_angle_modul = 40
 azimuth_angle_modul = 10
+m2 = 500
 
+#Apparent Extraterrestrial Solar Insolation 
+# Verwendet Formel aus Excel-Sheet Engineering New Mexico Resource Network 
+# Verluste der Extraterrestrischen Starhlung von durchschnittlich 1324,67 W/m2
+# 1160+75*SIN(0.017214206 * (julian_day - 275)))
+
+#Optical depth
+# Verwendet Formel aus Excel-Sheet Engineering New Mexico Resource Network 
+# Verluste der Strahlung durch die Dicker der Atmosphere
+# 0.174 + 0.035 * SIN(0.017214206 * (julian_day - 100)))
+
+#AirMassRatio
+#The Air Mass is the path length which light takes through the atmosphere normalized to the shortest possible path length (that is, when the sun is directly overhead).
+=abs(1/SIN(2 * pi/360 * solar_altitude))
+j = abs(-5)
+j
 
 sedn_slpc <- read_delim(path_sedn_slpc, delim = ",")
 
@@ -112,6 +128,8 @@ x <- sedn_slpc %>%
     utc_timestamp >= "1995-01-01 00:00:00",
     utc_timestamp <= "2015-12-31 24:00:00"
   ) %>% 
+  mutate(aesi = 1160 + 75 * sin(0.017214206 * (daynumber - 275))) %>% 
+  mutate(optical_depth = 0.174 + 0.035 * sin(0.017214206 * (daynumber - 100))) %>% 
   mutate(dec = -23.45 * cos(0.017453 * 360 * (daynumber + 10) / 365)) %>% 
   mutate(zeitgl = 60 * (-0.171 * sin(0.0337 * daynumber + 0.465) - 0.1299 * sin(0.01787 * daynumber  - 0.168))) %>% 
   mutate(stundenwinkel = 15*(hour(utc_timestamp) + (minute(utc_timestamp)/60) - ((15 - longitude)/15)- 12 + (zeitgl / 60))) %>% 
@@ -120,18 +138,21 @@ x <- sedn_slpc %>%
   mutate(cos_azimut = -((sin_d(latitude) * sin_sonnenhoehe) - (sin_d(dec))) / (cos_d(latitude) * sin_d(acos_d(sin_sonnenhoehe)))) %>% 
   mutate(azimut = ifelse((hour(utc_timestamp) + (minute(utc_timestamp) / 60)) <= 12 + ((15 - longitude) / 15) - (zeitgl / 60) , acos_d(cos_azimut) , 360 - (acos_d(cos_azimut)))) %>%  
   mutate(zenith_angle = 90 - sonnenhoehe) %>% 
+  mutate(air_mass_ratio = abs (1 / sin(2 * pi / 360 * sonnenhoehe))) %>%
   mutate(angle_of_incidance = (cos_d(sonnenhoehe) * cos_d(azimut - azimuth_angle_modul) * sin_d(tilt_angle_modul)) + (sin_d(sonnenhoehe) * cos_d(tilt_angle_modul))) %>% 
-  mutate(IAM = 1 + (-0.0019386 * aoi) + (0.00025854 * ((aoi)^2)) + -0.000011229 * ((aoi)^3) + 0.00000019962 * ((aoi) ^ 4) + -0.0000000012818 * ((aoi)^5)) %>% 
-  mutate(direct_radiation_pv = ifelse(sonnenhoehe > 0, (radiation_direct_horizontal * (sin_d(sonnenhoehe + 10) / sin_d(sonnenhoehe))) * IAM,0)) %>% 
-  mutate(diffuse_radiation_pv = radiation_diffuse_horizontal * 1/2 * (1 + cos_d(sonnenhoehe))) %>% 
+  mutate(IAM = 1 + (-0.0019386 * angle_of_incidance) + (0.00025854 * ((angle_of_incidance)^2)) + -0.000011229 * ((angle_of_incidance)^3) + 0.00000019962 * ((angle_of_incidance) ^ 4) + -0.0000000012818 * ((angle_of_incidance)^5)) %>% 
+  mutate(direct_radiation_pv = ifelse(sonnenhoehe > 0, (radiation_direct_horizontal/1000 * (sin_d(sonnenhoehe + 10) / sin_d(sonnenhoehe))) * IAM,0)) %>% 
+  mutate(diffuse_radiation_pv = radiation_diffuse_horizontal/1000 * 1/2 * (1 + cos_d(sonnenhoehe))) %>% 
   mutate(reflective_radiation_pv = (direct_radiation_pv + diffuse_radiation_pv) * 0.5 * (1 - cos_d(sonnenhoehe)) * 0.2) %>% 
-  mutate(solar_watt = ((direct_radiation_pv + diffuse_radiation_pv + reflective_radiation_pv) / 1000) * (-0.583 * temperature + 115)/100) %>% 
-  select(-zeitgl, -stundenwinkel, -sonnenhoehe,-cos_azimut,-angle_of_incidance, - direct_radiation_pv, -diffuse_radiation_pv,-reflective_radiation_pv, -dec) %>% 
+  mutate(solar_watt = (((direct_radiation_pv + diffuse_radiation_pv + reflective_radiation_pv)) * (-0.583 * temperature + 115)/100)) %>% 
+  #select(-zeitgl, -stundenwinkel, -sonnenhoehe,-cos_azimut,-angle_of_incidance, - direct_radiation_pv, -diffuse_radiation_pv,-reflective_radiation_pv, -dec) %>% 
   filter(
     utc_timestamp >= "2014-01-01 00:00:00",
-    utc_timestamp <= "2015-12-31 24:00:00"
-  )
-
+    utc_timestamp <= "2014-12-31 24:00:00"
+    
+  ) %>% 
+  summarise(kwh = sum(solar_watt), kwh_old = sum((radiation_direct_horizontal + radiation_diffuse_horizontal) / 1000) )
+  
   
 y = 436.6431 * sin_d((41.378 + 10))/sin_d(41.378)
 y
