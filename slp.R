e84a2667feec64023bfd512fe8fc2e02e1bafb45
j0gabonit0 <s-bandura@posteo.de>
2021-01-19 18:13:12 +0100

    lc

--- slp.R
+++ slp.R
@@ -43,3 +43,48 @@ sedn_slpc <- slpc_r %>%
     write_csv("C:/Users/corvi/Nextcloud/17_solar_dashbord/solar-dashbord-business/proof.csv")
   
   #colnames(sedn_slp_werk1)[2] <- "consumw1"
+  
+  
+  ### Lastgang nur kwH pro 15 Minuten - umwandeln und joinen mit Datengrundlage
+  
+  
+  x <- x <- read.csv("/home/sascha/Nextcloud/17_solar_dashbord/solar-dashbord-business/Lastgang/h0.csv", sep = ",")
+  y <- sum(x$kw)
+  
+  sedn_slpc_old <- read_csv("Datengrundlage/solar-nrw-nuts-small.csv")
+  
+  sedn_slpc_old <- sedn_slpc_old %>% 
+    na.omit()
+  
+  sedn_slpc_old <- sedn_slpc_old %>% 
+    mutate(u1 = utc_timestamp %>% as.character() %>% substr(6, 19)) 
+
+   
+
+  sedn_slpc <- x %>% 
+    mutate(date_full = seq.POSIXt(
+      ymd_hms("2021-01-01 00:00:00"),
+      ymd_hms("2021-12-31 23:45:00"),
+      by = "15 mins"
+    )) %>%
+    mutate(date_full = as.POSIXct(date_full, format = "%Y-%m-%d %H:%M:%S")) %>%
+    group_by(date = floor_date(date_full, unit = "hour")) %>%
+    summarise(kwh = sum(kwh) ) %>%
+    mutate(date = as.POSIXct(date, format = "%Y-%m-%d %H:%M:%S")) %>%
+    mutate(u1 = date %>% as.character() %>% substr(6, 19)) %>%
+    right_join(sedn_slpc_old, by = "u1")  %>%
+    select(-date, -consumw1, -X1,-X,-u1) %>%
+    rename(consumw1 = kwh) %>% 
+    na.omit()
+  
+  write.csv(sedn_slpc,"Datengrundlage/solar-nrw-nuts-small-new.csv", fileEncoding = 'UTF-8', row.names = FALSE)
+  
+  
+  
+c <- sum(sedn_slpc$consumw1) / 20
+  
+c <- sum(sedn_slpc_old$consumw1) / 20
+  
+  
+  
+
